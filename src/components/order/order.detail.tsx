4370246a1afdb3b3b278d1f3498413b353074c63
phonghh <longtam20015@gmail.com>
2024-12-27 19:15:47 +0700

    #82 Bài tập checkout page

--- src/components/order/order.detail.tsx
+++ src/components/order/order.detail.tsx
@@ -1,18 +1,35 @@
 import { deleteBookLocalStorage } from "@/services/book.service";
-import { DeleteOutlined } from "@ant-design/icons";
-import { Col, Divider, Form, FormProps, Image, InputNumber, Row } from "antd";
+import { DeleteOutlined, SmileOutlined } from "@ant-design/icons";
+import {
+  Button,
+  Col,
+  Divider,
+  Form,
+  FormProps,
+  Image,
+  Input,
+  InputNumber,
+  Radio,
+  Result,
+  Row,
+} from "antd";
 import { useForm } from "antd/lib/form/Form";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useCurrentApp } from "../context/app.context";
+import OrderStep from "./order.step";
+import TextArea from "antd/lib/input/TextArea";
+import { Link } from "react-router-dom";
 
 interface FieldType {
-  quantity?: number;
-  total?: number;
-  _id: string;
+  address?: string;
+  fullName?: string;
+  method: "COD" | "BANKING";
+  phone?: string;
 }
 const OrderDetail = () => {
   const [form] = useForm();
-  const { carts, setCarts } = useCurrentApp();
+  const [current, setCurrent] = useState<number>(0);
+  const { carts, setCarts, user } = useCurrentApp();
 
   const total = useMemo(() => {
     return carts?.reduce((acc, cart) => {
@@ -29,6 +46,10 @@ const OrderDetail = () => {
 
     if (newCarts) {
       setCarts(newCarts);
+    }
+
+    if (newCarts.length === 0) {
+      setCurrent(0);
     }
   };
 
@@ -56,167 +77,230 @@ const OrderDetail = () => {
 
   return (
     <>
-      <div className="w-full bg-gray-200">
-        <div className="p-10">
-          <Form
-            name="basic"
-            onFinish={onFinish}
-            form={form}
-            onFieldsChange={onFieldChange}
-          >
-            <Row gutter={12}>
-              <Col span={18}>
-                <Row gutter={[12, 12]}>
-                  {carts?.map((cart) => {
-                    return (
-                      <Col span={24} key={cart._id}>
-                        <div className="p-5 bg-white flex justify-between items-center border-solid rounded-lg border-white">
-                          <Col>
-                            <div>
-                              <Image
-                                width={80}
-                                src={baseUrl + cart.detail.thumbnail}
-                                preview={false}
-                              />
-                            </div>
-                          </Col>
-                          <Col>
-                            <div>
-                              <p className="text-[13px] capitalize break-words min-w-[200px] max-w-[200px]">
-                                {cart.detail.mainText}
-                              </p>
-                            </div>
-                          </Col>
-                          <Col>
-                            <div className="flex items-center justify-center space-x-2 ">
-                              <div className="flex justify-center items-center space-x-2 min-w-[140px]">
-                                <span className="text-[14px]">
-                                  {new Intl.NumberFormat("vi-VN", {
-                                    style: "currency",
-                                    currency: "VND",
-                                  }).format(cart.detail.price)}
-                                </span>
-                                <Form.Item
-                                  name={["quantity", cart?._id]}
-                                  noStyle={true}
-                                  initialValue={cart.quantity}
-                                >
-                                  <InputNumber min={1} max={1000} />
-                                </Form.Item>
-                              </div>
-                            </div>
-                          </Col>
-                          <Col>
-                            <div className="flex items-center space-x-2 ">
-                              <span className="text-[14px]">Tổng: </span>
-                              <span className="text-[14px]">
-                                {new Intl.NumberFormat("vi-VN", {
-                                  style: "currency",
-                                  currency: "VND",
-                                }).format(cart.detail.price * cart.quantity)}
-                              </span>
-                            </div>
-                          </Col>
-                          <Col>
-                            <div>
-                              <DeleteOutlined
-                                className="text-pink-500 cursor-pointer"
-                                onClick={() => handleDeleteBook(cart._id)}
-                              />
-                            </div>
-                          </Col>
+      <div className="w-full h-[100vw] bg-gray-200">
+        <div className="flex flex-col p-10 space-y-5">
+          <div className="bg-white p-5 border-solid rounded-lg border-white">
+            <OrderStep
+              current={current}
+              setCurrent={setCurrent}
+              cartLength={carts?.length}
+            />
+          </div>
+          <div className="">
+            <Form
+              name="basic"
+              layout="vertical"
+              onFinish={onFinish}
+              form={form}
+              onFieldsChange={onFieldChange}
+            >
+              <Row gutter={12}>
+                <Col span={16}>
+                  {carts?.length !== 0 ? (
+                    <Row gutter={[12, 12]}>
+                      {carts?.map((cart) => {
+                        return (
+                          <Col span={24} key={cart._id}>
+                            <div className="p-5 bg-white flex justify-between items-center border-solid rounded-lg border-white">
+                              <Col>
+                                <div>
+                                  <Image
+                                    width={80}
+                                    src={baseUrl + cart.detail.thumbnail}
+                                    preview={false}
+                                  />
+                                </div>
+                              </Col>
+                              <Col>
+                                <div>
+                                  <p className="text-[13px] capitalize break-words min-w-[200px] max-w-[200px]">
+                                    {cart.detail.mainText}
+                                  </p>
+                                </div>
+                              </Col>
+                              <Col>
+                                <div className="flex items-center justify-center space-x-2 ">
+                                  <div className="flex justify-center items-center min-w-[140px] space-x-2">
+                                    <span className="text-[14px]">
+                                      {new Intl.NumberFormat("vi-VN", {
+                                        style: "currency",
+                                        currency: "VND",
+                                      }).format(cart.detail.price)}
+                                    </span>
+                                    {current === 0 ? (
+                                      <Form.Item
+                                        name={["quantity", cart?._id]}
+                                        noStyle={true}
+                                        initialValue={cart.quantity}
+                                      >
+                                        <InputNumber min={1} max={1000} />
+                                      </Form.Item>
+                                    ) : (
+                                      <div className="flex justify-center items-center space-x-1 min-w-[100px]">
+                                        <span className="text-[14px]">
+                                          Số lượng:
+                                        </span>
+                                        <span>{cart.quantity}</span>
+                                      </div>
+                                    )}
+                                  </div>
+                                </div>
+                              </Col>
 
-                          {/* <div>
-                            <Image
-                              width={80}
-                              src={baseUrl + cart.detail.thumbnail}
-                              preview={false}
-                            />
-                          </div>
-                          <div>
-                            <p className="text-[13px] capitalize break-words min-w-[200px] max-w-[200px]">
-                              {cart.detail.mainText}
-                            </p>
-                          </div>
-                          <div className="flex items-center justify-center space-x-2 ">
-                            <div className="flex justify-center items-center space-x-2 min-w-[140px]">
-                              <span className="text-[14px]">
-                                {new Intl.NumberFormat("vi-VN", {
-                                  style: "currency",
-                                  currency: "VND",
-                                }).format(cart.detail.price)}
-                              </span>
-                              <Form.Item
-                                name={["quantity", cart?._id]}
-                                noStyle={true}
-                                initialValue={cart.quantity}
-                              >
-                                <InputNumber min={1} max={1000} />
-                              </Form.Item>
+                              <Col>
+                                <div className="flex items-center space-x-2 ">
+                                  <span className="text-[14px]">Tổng: </span>
+                                  <span className="text-[14px]">
+                                    {new Intl.NumberFormat("vi-VN", {
+                                      style: "currency",
+                                      currency: "VND",
+                                    }).format(
+                                      cart.detail.price * cart.quantity
+                                    )}
+                                  </span>
+                                </div>
+                              </Col>
+                              <Col>
+                                <div>
+                                  <DeleteOutlined
+                                    className="text-pink-500 cursor-pointer"
+                                    onClick={() => handleDeleteBook(cart._id)}
+                                  />
+                                </div>
+                              </Col>
                             </div>
-                          </div>
-                          <div className="flex items-center space-x-1 justify-between">
-                            <span className="text-[14px]">Tổng: </span>
-                            <span className="text-[14px]">
-                              {new Intl.NumberFormat("vi-VN", {
-                                style: "currency",
-                                currency: "VND",
-                              }).format(cart.detail.price * cart.quantity)}
-                            </span>
-                          </div>
-                          <div>
-                            <DeleteOutlined
-                              className="text-pink-500 cursor-pointer"
-                              onClick={() => handleDeleteBook(cart._id)}
-                            />
-                          </div> */}
-                        </div>
-                      </Col>
-                    );
-                  })}
-                </Row>
-              </Col>
-              <Col span={6}>
-                <div className="bg-white border-solid rounded-lg border-gray-100 p-5">
-                  <div className="flex justify-between items-center">
-                    <span className="text-sm">Tạm tính</span>
-                    <span className="text-sm">
-                      {new Intl.NumberFormat("vi-VN", {
-                        style: "currency",
-                        currency: "VND",
-                      }).format(total)}
-                    </span>
-                  </div>
-                  <div>
-                    <Divider />
-                  </div>
-                  <div className="flex justify-between">
-                    <span>Tổng tiền</span>
-                    <span className="text-lg text-orange-600">
-                      {new Intl.NumberFormat("vi-VN", {
-                        style: "currency",
-                        currency: "VND",
-                      }).format(total)}
-                    </span>
-                  </div>
-                  <div>
-                    <Divider />
-                  </div>
-                  <div>
-                    <button
-                      className={
-                        carts?.length > 0
-                          ? "w-full px-3 h-[40px] bg-orange-600 text-white rounded-sm border-none cursor-pointer hover:bg-orange-500"
-                          : "w-full px-3 h-[40px] bg-gray-600 text-white rounded-sm border-none "
+                          </Col>
+                        );
+                      })}
+                    </Row>
+                  ) : (
+                    <Result
+                      icon={<SmileOutlined />}
+                      title="Bạn hiện chưa có sản phẩm quay lại trang chủ để mua hàng"
+                      extra={
+                        <Button type="primary">
+                          <Link to={"/"}>Trang chủ</Link>
+                        </Button>
                       }
-                    >
-                      Mua Hàng ({carts?.length})
-                    </button>
+                    />
+                  )}
+                </Col>
+                <Col style={{ flex: 1 }}>
+                  <div className="flex flex-col space-y-2 bg-white border-solid rounded-lg border-gray-100 p-5">
+                    {current !== 0 && (
+                      <>
+                        <div>
+                          <Form.Item<FieldType>
+                            name={"method"}
+                            label={"Hình thức thanh toán:"}
+                            initialValue={"COD"}
+                          >
+                            <Radio.Group className="flex flex-col space-y-2">
+                              <Radio value={"COD"}>
+                                Thanh toán khi nhận hàng
+                              </Radio>
+                              <Radio value={"BANKING"}>
+                                Chuyển khoản ngân hàng
+                              </Radio>
+                            </Radio.Group>
+                          </Form.Item>
+
+                          <Form.Item<FieldType>
+                            name={"fullName"}
+                            label={"Họ tên"}
+                            initialValue={user?.fullName}
+                            rules={[
+                              {
+                                required: true,
+                                message: "Họ tên không được để trống",
+                              },
+                            ]}
+                          >
+                            <Input />
+                          </Form.Item>
+
+                          <Form.Item<FieldType>
+                            name={"phone"}
+                            label={"Số điện thoại"}
+                            initialValue={user?.phone}
+                            rules={[
+                              {
+                                required: true,
+                                message: "Số điện thoại không được để trống",
+                              },
+                            ]}
+                          >
+                            <Input />
+                          </Form.Item>
+
+                          <Form.Item<FieldType>
+                            name={"address"}
+                            label={"Địa chỉ nhận hàng"}
+                            initialValue={""}
+                            rules={[
+                              {
+                                required: true,
+                                message:
+                                  "Địa chỉ nhận hàng không được để trống",
+                              },
+                            ]}
+                          >
+                            <TextArea rows={4} />
+                          </Form.Item>
+                        </div>
+                      </>
+                    )}
+                    <div>
+                      <div className="flex justify-between items-center">
+                        <span className="text-sm">Tạm tính</span>
+                        <span className="text-sm">
+                          {new Intl.NumberFormat("vi-VN", {
+                            style: "currency",
+                            currency: "VND",
+                          }).format(total)}
+                        </span>
+                      </div>
+                      <div>
+                        <Divider />
+                      </div>
+                      <div className="flex justify-between">
+                        <span>Tổng tiền</span>
+                        <span className="text-lg text-orange-600">
+                          {new Intl.NumberFormat("vi-VN", {
+                            style: "currency",
+                            currency: "VND",
+                          }).format(total)}
+                        </span>
+                      </div>
+                      <div>
+                        <Divider />
+                      </div>
+                      <div>
+                        <button
+                          className={
+                            carts?.length > 0
+                              ? "w-full px-3 h-[40px] bg-orange-600 text-white rounded-sm border-none cursor-pointer hover:bg-orange-500"
+                              : "w-full px-3 h-[40px] bg-gray-600 text-white rounded-sm border-none "
+                          }
+                          disabled={carts?.length === 0}
+                          onClick={() => {
+                            if (current === 1) {
+                              form.submit();
+                            }
+                            setCurrent(1);
+                          }}
+                          type="button"
+                        >
+                          {current === 0 ? "Mua Hàng" : "Đặt Hàng"} (
+                          {carts?.length})
+                        </button>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </Col>
-            </Row>
-          </Form>
+                </Col>
+              </Row>
+            </Form>
+          </div>
         </div>
       </div>
     </>
